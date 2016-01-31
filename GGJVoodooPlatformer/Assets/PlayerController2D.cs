2ea45da639c47c6cce52de27a551429fc50cb183
Kyle Phillips <spaznur@gmail.com>
2016-01-31 13:14:01 -0800

    Working on new char controller

---
+++ GGJVoodooPlatformer/Assets/PlayerController2D.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController2D : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        var move = Input.GetAxis("Horizontal");
+
+        if (move > 0)
+        {
+            if(transform.localScale.x < 0f)
+            {
+                transform.localScale = new Vector3(-transform.localScale.x, transform.localScale.y, transform.localScale.z);
+            }
+        }
+        else if (move < 0)
+        {
+        }
+        else
+        {
+        }
+	}
+}
